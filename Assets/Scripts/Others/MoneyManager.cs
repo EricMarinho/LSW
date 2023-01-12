7b919b95393aaee9a728c2b961da8a0e9c169720
Eric Marinho (Ilidam) <ericmarinho@acad.ifma.edu.br>
2023-01-12 18:51:59 -0300

    feat: Added indicator of earning and losing money

--- Assets/Scripts/Others/MoneyManager.cs
+++ Assets/Scripts/Others/MoneyManager.cs
@@ -17,6 +17,7 @@ public class MoneyManager : MonoBehaviour
     #endregion
 
     public TMP_Text moneyText;
+    [SerializeField] private GameObject moneyChangeText;
     private PlayerController playerControllerInstance;
 
     private void Start()
@@ -29,12 +30,21 @@ public class MoneyManager : MonoBehaviour
     {
         playerControllerInstance.playerData.currentMoney += amount;
         moneyText.text = playerControllerInstance.playerData.currentMoney.ToString() + " coins";
+
+        GameObject moneyChangeTextInstance = Instantiate(moneyChangeText, moneyText.transform.position, Quaternion.identity, moneyText.transform.parent);
+        moneyChangeTextInstance.GetComponent<TMP_Text>().text = "+" + amount.ToString();
+        moneyChangeTextInstance.GetComponent<TMP_Text>().color = Color.green;
+
     }
 
     public void RemoveMoney(float amount)
     {
         playerControllerInstance.playerData.currentMoney -= amount;
         moneyText.text = playerControllerInstance.playerData.currentMoney.ToString() + " coins";
+
+        GameObject moneyChangeTextInstance = Instantiate(moneyChangeText, moneyText.transform.position, Quaternion.identity, moneyText.transform.parent);
+        moneyChangeTextInstance.GetComponent<TMP_Text>().text = "-" + amount.ToString();
+        moneyChangeTextInstance.GetComponent<TMP_Text>().color = Color.red;
     }
 
     public bool CanAfford(float amount)
